2306311a07466df90f283269d618ab377db86941
roshomon <roshomon@126.com>
2021-09-25 16:36:38 +0800

    完成layerControl折叠面板；尝试多选排列

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <LayerControl />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from "vue";
+import LayerControl from "./components/LayerControl.vue";
 
-export default {
-  name: 'App',
+export default defineComponent({
   components: {
-    HelloWorld
-  }
-}
+    LayerControl,
+  },
+});
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/layerControl.vue
+++ src/components/layerControl.vue
@@ -1,58 +1,71 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <a-collapse activeKey="1">
+    <a-collapse-panel key="1" header="电网">
+      <a-row>
+        <a-col :flex="1">
+          <a-checkbox
+            v-model:checked="checkAll"
+            :indeterminate="indeterminate"
+            @change="onCheckAllChange"
+          >
+            Check all
+          </a-checkbox>
+        </a-col>
+        <a-col :flex="4">
+          <a-checkbox-group v-model:value="checkedList" :options="plainOptions" />
+        </a-col>
+      </a-row>
+
+      <br />
+
+      <a-checkbox-group v-model:value="value">
+        <a-row>
+          <a-col :span="8">
+            <a-checkbox value="A">A1111111</a-checkbox>
+          </a-col>
+          <a-col :span="8">
+            <a-checkbox value="B">B1111</a-checkbox>
+          </a-col>
+          <a-col :span="8">
+            <a-checkbox value="C">C1</a-checkbox>
+          </a-col>
+          <a-col :span="8">
+            <a-checkbox value="D">D</a-checkbox>
+          </a-col>
+          <a-col :span="8">
+            <a-checkbox value="E">E</a-checkbox>
+          </a-col>
+        </a-row>
+      </a-checkbox-group>
+    </a-collapse-panel>
+  </a-collapse>
 </template>
-
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import { defineComponent, reactive, toRefs, watch } from "vue";
+const plainOptions = ["Apple", "Pear", "Orange"];
+export default defineComponent({
+  setup() {
+    const state = reactive({
+      indeterminate: true,
+      checkAll: false,
+      checkedList: ["Apple", "Orange"],
+    });
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+    const onCheckAllChange = (e) => {
+      Object.assign(state, {
+        checkedList: e.target.checked ? plainOptions : [],
+        indeterminate: false,
+      });
+    };
+
+    watch(
+      () => state.checkedList,
+      (val) => {
+        state.indeterminate = !!val.length && val.length < plainOptions.length;
+        state.checkAll = val.length === plainOptions.length;
+      }
+    );
+    return { ...toRefs(state), plainOptions, onCheckAllChange };
+  },
+});
+</script>
